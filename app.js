54d71a4cdd0359a16124d3b1fa1de6c85cf6fe77
LoyalEnv0y <77401698+LoyalEnv0y@users.noreply.github.com>
2021-09-30 16:03:41 +0300

    Implemented OOP to app.js
    Implemented OOP version of the app. This way it is easier to add eventListeners to buttons.
    Implemented addScoreAndCheck to handle both player's click button events.
    Renamed finalizer as endGame.
    Rewrote some comments.

--- app.js
+++ app.js
@@ -1,101 +1,98 @@
-const playerOneScore = document.querySelector(".playerOneScore");
-const playerTwoScore = document.querySelector(".playerTwoScore");
-const playerOneButton = document.querySelector(".button1");
-const playerTwoButton = document.querySelector(".button2");
-const resetButton = document.querySelector(".button3");
+const p1 = {
+    score: document.querySelector(".playerOneScore"),
+    button: document.querySelector(".playerOneBtn"),
+}
+const p2 = {
+    score: document.querySelector(".playerTwoScore"),
+    button: document.querySelector(".playerTwoBtn"),
+}
+
+const resetButton = document.querySelector(".resetBtn");
 const goal = document.querySelector("#goal");
 
 /**
- * Everytime the playerOneButton has been clicked it runs
- * @function isWinner to check if the game is finished or not.
- * If it is, than it runs the 
- * @function finalizer
+ * Whenever p1.button or p2.button gets clicked
+ * @function addScoreAndCheck runs.
  */
-playerOneButton.addEventListener('click', () => {
-    playerOneScore.textContent++;
+p1.button.addEventListener('click', () => addScoreAndCheck(p1, p2));
+p2.button.addEventListener('click', () => addScoreAndCheck(p2, p1));
 
-    if (isWinner(playerOneScore.innerHTML)) {
-        finalizer(playerOneScore, playerTwoScore);
-    }
-})
+/**
+ * When resetButton gets clicked both players' scores will
+ * be set to 0 and classes will be removed
+ * Also both players' buttons will be re enabled.
+ */
+resetButton.addEventListener('click', () => {
+    p1.score.innerHTML = "0";
+    p2.score.innerHTML = "0";
 
-// Same as playerOneButton event listener but for playerTwoButton.
-playerTwoButton.addEventListener('click', () => {
-    playerTwoScore.textContent++;
+    p1.button.disabled = false;
+    p2.button.disabled = false;
 
-    if (isWinner(playerTwoScore.innerHTML)) {
-        finalizer(playerTwoScore, playerOneScore);
-    }
+    p1.score.classList.remove('winner', 'loser', 'tie');
+    p2.score.classList.remove('winner', 'loser', 'tie');
 })
 
 /**
  * Everytime the goal is changed this code checks if a player has won 
- * the game or not and if they did, it finishes the game by calling
- * @function finalizer
- * 
- * It also handles some bugs that can be generated when user changes 
- * the goal whilst playing the game. 
- * 
- * It handles when;
- * a: User lowered goal and the new goal is lower or equal to 
- * any of the players' score. (it chooses the player with higher score)
- * b: User lowered goal and the new goal is equal to both players.
- * (when the game is a tie).
+ * the game or not and if they did, It checks who is the winner
+ * and if the game is a tie or not. It finishes the game by calling
+ * @function endGame
  */
 goal.addEventListener('change', () => {
-    if (isWinner(playerOneScore.innerHTML) || isWinner(playerTwoScore.innerHTML)) {
-        if (playerOneScore.innerHTML > playerTwoScore.innerHTML) {
-            finalizer(playerOneScore, playerTwoScore);
-        } else if (playerOneScore.innerHTML < playerTwoScore.innerHTML){
-            finalizer(playerTwoScore, playerOneScore);
+    let p1ScoreInt = parseInt(p1.score.textContent)
+    let p2ScoreInt = parseInt(p2.score.textContent)
+
+    if (isWinner(p1) || isWinner(p2)) {
+        if (p1ScoreInt > p2ScoreInt) {
+            endGame(p1, p2)
+        } else if (p2ScoreInt > p1ScoreInt) {
+            endGame(p2, p1)
         } else {
-            finalizer(playerOneScore, playerTwoScore, true);
+            endGame(p1, p2, true)
         }
     }
 })
 
 /**
- * Everytime resetButton gets clicked it sets both players scores to 0,
- * enables the score incrementing buttons,
- * removes the winner, loser and tie classes of the buttons.
+ * here we increment the pressed button's player's score and check
+ * if the game is finished or not.
+ * 
+ * @param {Element} scoringPlayer is the clicked buttons player.
+ * @param {Element} otherPlayer is the opposing player.
  */
-resetButton.addEventListener('click', () => {
-    playerTwoScore.innerHTML = "0";
-    playerOneScore.innerHTML = "0";
+function addScoreAndCheck(scoringPlayer, otherPlayer) {
+    scoringPlayer.score.textContent++;
 
-    playerOneButton.disabled = false;
-    playerTwoButton.disabled = false;
-
-    playerOneScore.classList.remove('winner', 'loser', 'tie');
-    playerTwoScore.classList.remove('winner', 'loser', 'tie');
-})
-
-/**
- * @param {number} playerScore is the score of the player that is being checked
- * @returns whether the playerScore is higher then goal or not.
- */
-function isWinner(playerScore) {
-    return parseInt(playerScore) >= parseInt(goal.value)
+    if (isWinner(scoringPlayer)) {
+        endGame(scoringPlayer, otherPlayer)
+    }
 }
 
 /**
- * @function finalizer adds winner and loser classes to the 
- * players that is passed in as arguments and
- * disables the score incrementing buttons.
- * 
- * @param {Element} winner is the leading player 
- * @param {Element} loser is the fallowing player
- * @param {Boolean} isTie is the identifier for if the game is a tie or not.
+ * @param {Element} player is the player who is being checked.
+ * @returns {boolean} whether the playerScore is higher then goal or not.
  */
-function finalizer(winner, loser, isTie = false) {
-    if (isTie) {
-        winner.classList.add('tie')
-        loser.classList.add('tie')
-    } else {
-        winner.classList.add('winner');
-        loser.classList.add('loser');
-    }
+function isWinner(player) {
+    return parseInt(player.score.textContent) >= parseInt(goal.value)
+}
 
-    playerOneButton.disabled = true;
-    playerTwoButton.disabled = true;
+/**
+ * finishes the game by disabling the buttons and adding classes
+ * to winners and losers. Adds tie class if the game is a tie.
+ * 
+ * @param {Element} winner is the leading player.
+ * @param {Element} loser is the fallowing player.
+ * @param {Boolean} isTie is the identifier for if the game is tie or not.
+ */
+function endGame(winner, loser, isTie = false) {
+    if (isTie) {
+        winner.score.classList.add('tie')
+        loser.score.classList.add('tie')
+    } else {
+        winner.score.classList.add('winner');
+        loser.score.classList.add('loser');
+    }
+    p1.button.disabled = true;
+    p2.button.disabled = true;
 }
